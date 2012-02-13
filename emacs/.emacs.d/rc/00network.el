94756778c9c3fcbb0a596a11fa811f211b55b9f7
kmr <kmr.clover@gmail.com>
2012-02-14 00:41:05 +0900

    del

--- emacs/.emacs.d/rc/00network.el
+++
@@ -1,5 +0,0 @@
-;; proxy
-;;(setq http-proxy-server "proxysv2.nic.nec.co.jp")
-;;(setq http-proxy-port 8080)
-;;(load-library "install-elisp")
-;;(setq install-elisp-repository-directory "~/.emacs.d/plugins/")
